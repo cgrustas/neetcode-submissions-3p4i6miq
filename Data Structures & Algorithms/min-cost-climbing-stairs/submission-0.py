574fdfb56eabd67987e8ab0e357a51c8b44ab00f
Coleman Grustas <chgrustas@gmail.com>
2026-06-23 06:15:30 -0700

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cache = [None] * len(cost)
+        total = 0
+        
+        def dfs(i):
+            if i >= len(cost):
+                return 0
+
+            if not cache[i]:
+                cache[i] = cost[i] + min(dfs(i + 1), dfs(i + 2))
+                
+            return cache[i]
+
+        # You may choose to start at the index 0 or the index 1 floor.
+        return min(dfs(0), dfs(1))
