63f55de0dddaed2e931bde28cb1453fb617d9928
Coleman Grustas <chgrustas@gmail.com>
2026-06-24 05:58:24 -0700

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # case 1: rob this house
+            # nums[i] + dfs(i + 2)
+        # case 2: do not rob this house
+            # dfs(i + 1)
+        
+        memo = [None] * len(nums)
+    
+        def dfs(i):
+            if i >= len(nums): 
+                return 0
+            
+            if memo[i] is None: 
+                memo[i] = max(nums[i] + dfs(i + 2), dfs(i + 1))
+                
+            return memo[i]
+
+        return dfs(0)
